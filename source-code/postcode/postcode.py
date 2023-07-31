1e70cf2fcfa23b5a172a74709533780c6ea5b626
Tong LI <tl10@sanger.ac.uk>
2023-07-31 13:30:43 +0000

    defaulting the codebook to starfish format

--- source-code/postcode/postcode.py
+++ source-code/postcode/postcode.py
@@ -3,39 +3,37 @@
 
 import numpy as np
 import pandas as pd
+import fire
+from pathlib import Path
 
 from starfish.core.codebook.codebook import Codebook
-from starfish.core.spots.DecodeSpots.trace_builders import build_spot_traces_exact_match
-from starfish.core.types import SpotFindingResults
 from postcode.decoding_functions import decoding_function, decoding_output_to_dataframe
 
-def main(spots: SpotFindingResults, codebook: str, keep_noises=True) -> pd.DataFrame:
+def decode(spot_locations_p: str, spot_profile_p: str, codebook_p: str, keep_noises=True) -> pd.DataFrame:
     """
     Decodes spots using the Postcode algorithm.
 
     Args:
-        spots (SpotFindingResults): A SpotFindingResults object containing the spots to be decoded.
-        codebook (str): A JSON string representing the codebook to be used for decoding.
+        spot_locations_p (str): A file path to pandas DataFrame containing the spot locations.
+        spot_profile_p (str): A file path to numpy array containing the spot profiles (N x C x R).
+        codebook (str): A starfish Codebook object containing the barcode sequences.
         keep_noises (bool, optional): Whether to keep spots that were classified as 'background' or 'infeasible'.
             Defaults to True.
 
     Returns:
         pd.DataFrame: A pandas DataFrame containing the decoded spots and their locations.
     """
-    codebook = Codebook.from_json(codebook)
+    stem = Path(spot_locations_p).stem
+    spot_locations = pd.read_csv(spot_locations_p)
+    spot_profile = np.load(spot_profile_p, allow_pickle=True)
+    codebook = Codebook.open_json(codebook_p)
+    assert spot_locations.shape[0] == spot_profile.shape[0]
 
-    # Format starfish spots for use in postcode
-    bd_table = build_spot_traces_exact_match(spots)
-    spots_s = np.swapaxes(bd_table.data, 1, 2)
-    spots_loc_s = pd.DataFrame(columns=['X', 'Y', 'Z'])
-    spots_loc_s['X'] = np.array(bd_table.x)
-    spots_loc_s['Y'] = np.array(bd_table.y)
-    spots_loc_s['Z'] = np.array(bd_table.z)
     barcodes_01 = np.swapaxes(np.array(codebook), 1, 2)
     K = barcodes_01.shape[0]
 
     # Decode using postcode
-    out = decoding_function(spots_s, barcodes_01, print_training_progress=True)
+    out = decoding_function(spot_profile, barcodes_01, print_training_progress=False)
 
     # Reformat output into pandas dataframe
     df_class_names = np.concatenate((codebook.target.values,
@@ -48,8 +46,13 @@ def main(spots: SpotFindingResults, codebook: str, keep_noises=True) -> pd.DataF
     df_class_codes = np.concatenate((barcodes_AGCT, ['NA', '0000', 'NA']))
     decoded_spots_df = decoding_output_to_dataframe(out, df_class_names, df_class_codes)
 
-    decoded_df_s = pd.concat([decoded_spots_df, spots_loc_s], axis=1)
+    decoded_df_s = pd.concat([decoded_spots_df, spot_locations], axis=1)
     if keep_noises:
-        return decoded_df_s
-    # Remove infeasible and background codes
-    return decoded_df_s[~np.isin(decoded_df_s['Name'], ['background', 'infeasible'])].reset_index(drop=True)
+        decoded_df_s.to_csv(f"{stem}_decoded_spots.csv", index=False)
+    else:
+        # Remove infeasible and background codes
+        decoded_df_s[~np.isin(decoded_df_s['Name'], ['background', 'infeasible'])].reset_index(drop=True).to_csv(f"{stem}_decoded_spots.csv", index=False)
+
+
+def main():
+    fire.Fire(decode)
